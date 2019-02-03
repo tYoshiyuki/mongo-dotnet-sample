6bd10534f3c86be940e5bf6f94b655757e908601
yTsunomoto <wasiwasimaru@gmail.com>
2019-02-03 16:45:32 +0900

    update

---
+++ MongoSample/Models/SampleCollection.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoSample.Models
+{
+    public class SampleCollection
+    {
+        public ObjectId Id { get; set; }
+        public string Data { get; set; }
+        public string Val { get; set; }
+    }
+}
---
+++ MongoSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using MongoSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.AppSettings["MongoDBConnection"];
+            var client = new MongoClient(connectionString);
+
+            var db = client.GetDatabase(ConfigurationManager.AppSettings["Db"]);
+            var collection = db.GetCollection<SampleCollection>("sampleCollection");
+
+            var sc = new SampleCollection
+            {
+                Data = "でーた",
+                Val = "ばりゅー"
+            };
+
+            collection.InsertOne(sc);
+
+            var scs = collection.Find(_ => true).ToList();
+            foreach(var c in scs)
+            {
+                Console.WriteLine($"{c.Id}/{c.Data}");
+            }
+        }
+    }
+}
